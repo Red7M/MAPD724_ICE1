c7dd27c36f834f388068bfbc6f4e49ab6af800a7
Red7M <reda.mehali@gmail.com>
2021-02-12 22:31:06 -0500

    Initial Commit

---
+++ MAPD726_ICE_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MAPD726_ICE_1
+//
+//  Created by Reda Mehali on 2/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
